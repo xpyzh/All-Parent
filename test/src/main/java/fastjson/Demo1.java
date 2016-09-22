55e27bb8c8c1f2450b5766a0d88669deba8fd67e
youzhihao <380104026@qq.com>
2016-09-22 14:36:14 +0800

    1. 测试fastjson的对象转化json字符串的坑 2.增加父pom构建属性,默认使用1.8的jdk

--- test/src/main/java/fastjson/Demo1.java
+++ test/src/main/java/fastjson/Demo1.java
@@ -1,11 +1,15 @@
 package fastjson;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by hzyouzhihao on 2016/9/22.
+ * 结论,使用fastjson进行object转json字符串的时候,如果有isXXX,getXXX且非void返回值方法默认都会调用
+ * 因此实体对象,不要在里面加任何is,get的非getter,setter方法
  */
+
 public class Demo1 {
 
     private boolean isExist; //不要使用is开头来命名本地
@@ -27,25 +31,32 @@ public class Demo1 {
         return flag;
     }
 
+    public String getAbc() {
+        return abc;
+    }
+
+    public void setAbc(String abc) {
+        this.abc = abc;
+    }
+
     public void setFlag(boolean flag) {
         this.flag = flag;
     }
 
-    public void getNullException() {
-       System.out.println(123);
+
+    public ArrayList getNullException() {
+        return new ArrayList<>();
+    }
+
+    public String isTest() {
+        return "isTest";
     }
 
     public static void main(String[] args) {
-//        Demo1 demo1 = new Demo1();
-//        demo1.setIsExist(true);
-//        demo1.setFlag(true);
-//        String jsonStr1 = JSON.toJSONString(demo1);
-//        String jsonStr2 = JSONObject.toJSONString(demo1);
-//        System.out.println(jsonStr1);
-//        System.out.println(jsonStr2);
-        EmailMessageBean emailMessageBean=new EmailMessageBean();
-        System.out.println(JSON.toJSONString(emailMessageBean));
-
+        Demo1 demo1 = new Demo1();
+        demo1.setIsExist(true);
+        demo1.setFlag(true);
+        System.out.println(JSONObject.toJSONString(demo1));
     }
 
 }
