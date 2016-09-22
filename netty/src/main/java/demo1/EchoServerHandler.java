d4620b20ed7bdb8cebc45a2b83813f82a99246e8
hzyouzhihao <hzyouzhihao@corp.netease.com>
2016-09-22 17:29:58 +0800

    netty: 1.重写discard服务器和客户端

--- netty/src/main/java/demo1/EchoServerHandler.java
+++ netty/src/main/java/demo1/EchoServerHandler.java
@@ -2,6 +2,7 @@ package demo1;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -22,9 +23,7 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     }
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-      //ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
-        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);
-
+      ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
     }
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
