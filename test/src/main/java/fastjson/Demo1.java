e2eacf44bb36610035d8fecab8b3281c07c65080
youzhihao <380104026@qq.com>
2016-09-22 13:55:34 +0800

    netty相关demo

---
+++ test/src/main/java/fastjson/Demo1.java
@@ -0,0 +1,49 @@
+package fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by hzyouzhihao on 2016/9/22.
+ */
+public class Demo1 {
+
+    private boolean isExist; //不要使用is开头来命名本地
+
+    //boolean类型的变量的get方法不要使用is开头
+    private boolean flag;
+
+    private String abc;
+
+    public boolean isExist() {
+        return isExist;
+    }
+
+    public void setIsExist(boolean isExist) {
+        this.isExist = isExist;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public void getNullException() {
+       System.out.println(123);
+    }
+
+    public static void main(String[] args) {
+        Demo1 demo1 = new Demo1();
+        demo1.setIsExist(true);
+        demo1.setFlag(true);
+        String jsonStr1 = JSON.toJSONString(demo1);
+        String jsonStr2 = JSONObject.toJSONString(demo1);
+        System.out.println(jsonStr1);
+        System.out.println(jsonStr2);
+
+    }
+
+}
