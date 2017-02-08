a74324bdcd92c7be4c0a1224aa2c505d5a63b43f
youzhihao <380104026@qq.com>
2017-02-08 11:09:19 +0800

    配置新的博客地址

--- test/src/main/java/Test.java
+++ test/src/main/java/Test.java
@@ -4,7 +4,12 @@
 public class Test {
 
     public static void main(String[] args) throws Exception {
+        //demo1();
+        //demo2();
 
+    }
+
+    public static void demo1() throws Exception {
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             @Override
             public void run() {
@@ -15,6 +20,22 @@ public class Test {
             Thread.currentThread().sleep(1000);
             System.out.println("程序结束");
         }
+    }
+
+    /**演示java基本类型是传值还是传引用,结果：java基本类型是传值*/
+    public static void demo2() {
+        int num = 2;
+        demo2_1(num);
+        System.out.println(num);
+    }
+
+    public static void demo2_1(int num) {
+        num++;
+        System.out.println(num);
+    }
+
+    public static void demo3() {
+        int nums[] = {1, 2, 3, 4, 5};
     }
 
 }
